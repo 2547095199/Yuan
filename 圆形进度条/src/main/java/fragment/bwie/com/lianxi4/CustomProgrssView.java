3ce7c9687d9897e460c71561c2e24c029695bd15
2547095199 <2547095199@qq.com>
2017-11-01 20:02:41 +0800

    这是圆形进度条

--- 圆形进度条/src/main/java/fragment/bwie/com/lianxi4/CustomProgrssView.java
+++ 圆形进度条/src/main/java/fragment/bwie/com/lianxi4/CustomProgrssView.java
@@ -16,6 +16,7 @@ public class CustomProgrssView extends View{
 
     private boolean runing = true;
     private int progress =  0;
+    //定义一个画笔
     private Paint paint;
 
     public CustomProgrssView(Context context) {
@@ -24,9 +25,13 @@ public class CustomProgrssView extends View{
 
     public CustomProgrssView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        //创建一个画笔
         paint = new Paint();
+        //抗锯齿
         paint.setAntiAlias(true);
+        //设置画笔的颜色
         paint.setColor(Color.RED);
+        //设置画笔，填充是空心的
         paint.setStyle(Paint.Style.STROKE);
         new Thread(new Runnable() {
             @Override
@@ -38,6 +43,7 @@ public class CustomProgrssView extends View{
                     }
                     System.out.println("progress" + progress);
                     progress += 10;
+                    //子线程刷新，系统调用onDraw方法
                     postInvalidate();
                     try {
                         Thread.sleep(100);
@@ -56,18 +62,26 @@ public class CustomProgrssView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //获取当前view的宽度
         int x = getWidth() / 2;
         int y = getHeight() / 2;
         int radius = 100;
+        //设置画笔的粗细
         paint.setStrokeWidth(30);
+        //定义一个区域
         RectF rectF = new RectF(x - radius, y - radius, x + radius, y + radius);
+       //true从中心点开始画  false 中心点不现实    。画弧
         canvas.drawArc(rectF,-90,progress,false,paint);
         int text = (int) ((float)progress/360 * 100);
+        //测量字符串的宽度
         float textWidth = paint.measureText(text + "%");
         Rect recttext = new Rect();
+        //获取字符串的高度
         paint.getTextBounds(text+"%",0,(text+"%").length(),recttext);
+        //文字大小
         paint.setTextSize(30);
         paint.setStrokeWidth(1);
+        //控制文字位置
         canvas.drawText(text+"%",x-textWidth/2,y+recttext.height()/4,paint);
 
     }
